927c15be359e5d8a26dbb10b0f3e49895dcb13f6
AtlasL <loxingle1@gmail.com>
2024-02-21 18:12:08 +0800

    Update main.py

--- tests/v0.0.1/main.py
+++ tests/v0.0.1/main.py
@@ -2,5 +2,5 @@
 from sheep import baa
 baa.create()
 baa.set_background(image='https://i.pinimg.com/736x/cf/92/99/cf9299fb011122c844c904e66c0833cf.jpg', image_repeat=False, image_size='cover')
-baa.write(language='zh', style='H1', content='This is the first Sheep site built with Sheep and atm it\'s horrible', size='29px', font='Roboto', colour='yellow')
+baa.write(language='zh', style='H1', content='This is the first Sheep site built with Sheep and atm it\'s horrible anyways https://github.com/AtlasL1/Sheep', size='29px', font='Roboto', colour='yellow')
 baa.view_page()
