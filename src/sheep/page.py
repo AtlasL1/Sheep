28a98ad5db6b8d215e816e86faf0c5e9eb132cba
AtlasL <loxingle1@gmail.com>
2024-02-26 21:10:44 +0800

    Update page.py

--- src/sheep/page.py
+++ src/sheep/page.py
@@ -42,7 +42,15 @@ def set_background(colour='white', image='', image_repeat='True', image_attachme
             else:
                 file.write(f'<style>\nbody {{background-color: {colour};}}\n</style>\n')
 
-def write(style='p', size='', font='Times New Roman', content='DEFAULT TEXT', colour='black', language='en'):
+def style_links(text_colour='blue', visited_colour='purple', hover_colour='darkblue', active_colour='blue'):
+    global global_file_name
+    if global_file_name is None:
+        raise ValueError('File not found.')
+    else:
+        with open(f'{global_file_name}.html', 'a') as file:
+            file.write(f'<style>\na:link {{color: {text_colour};}}\na:visited {{color: {visited_colour};}}\na:hover {{color: {hover_colour};}}\na:active {{color: {active_colour};}}</style>\n')
+
+def write(style='p', size='', font='Times New Roman', content='DEFAULT TEXT', colour='black', language='en', link_text='', url='', new_tab='False'):
     styles = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'P']
     global global_file_name
     if global_file_name is None:
@@ -51,8 +59,16 @@ def write(style='p', size='', font='Times New Roman', content='DEFAULT TEXT', co
         if style in styles:
             if style == 'p':
                 size = '12px'
+            if link_text in content:
+                if new_tab is True:
+                    content = content.replace(link_text, f'<a href="{url}" target="_blank">{link_text}</a>')
+                else:
+                    content = content.replace(link_text, f'<a href="{url}">{link_text}</a>')
+                link_text = ''
+            else:
+                content += f' <a href="{url}">{link_text}</a>'
             with open(f'{global_file_name}.html', 'a') as file:
-                file.write(f'<body>\n<{style.lower()} lang=\'{language}\' style=\'color:{colour}; font-size: {size}; font-family: "{font}"\'>{content}</{style.lower()}>\n</body>')
+                file.write(f'<body>\n<{style.lower()} lang=\'{language}\' style=\'color:{colour}; font-size: {size}; font-family: "{font}"\'>{content}</{style.lower()}>\n</body>\n')
         else:
             raise ValueError('Unknown text style.')
 
@@ -63,10 +79,3 @@ def view_page():
     else:
         file_path = f'{global_file_name}.html'
         webbrowser.open(file_path, new=2)
-
-create()
-set_title('hi')
-set_icon('https://cdn.discordapp.com/avatars/860794014764105729/404b7a92d2f882705e5b864dcdd807bf.webp?size=1024&format=webp&width=0&height=320')
-set_background(image='https://i.pinimg.com/736x/cf/92/99/cf9299fb011122c844c904e66c0833cf.jpg', image_repeat=False, image_size='cover')
-write(language='zh', style='H1', content='This is the first Sheep site built with Sheep and atm it\'s horrible', size='29px', font='Roboto', colour='yellow')
-view_page()
